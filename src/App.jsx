ac133273e5508c574d916560a5550943a935f8cc
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2024-11-15 18:40:41 +0500

    Design Updates

--- src/App.jsx
+++ src/App.jsx
@@ -71,57 +71,61 @@ function App() {
     saveToLS()
   }
 
-  
+
 
   return (
     <>
-      <NavBar />
-      <div className="container mx-auto my-5 p-5 rounded-xl bg-slate-200 text-slate-700 min-h-[80vh]">
+      <div className='min-h-screen bg-cyan-950'>
+        <NavBar />
 
-        <div className="addTodo ">
+        <div className="container mx-auto my-5 p-5 rounded-xl bg-cyan-900 opacity-85 min-h-[80vh] drop-shadow-2xl">
 
-          <h1 className=' m-4 font-bold text-2xl max-sm:m-0 font-serif text-center'>Z-Task - Manage your tasks at one place</h1>
+          <div className="addTodo  text-cyan-100">
 
-          <div className="flex justify-center m-4 max-sm:mx-0 max-sm:my-2">
-            <h2 className='text-xl font-bold mx-2 max-sm:text-base max-sm:mx-1 max-sm:hidden'>Add a Todo</h2>
+            <h1 className=' m-4 font-bold text-2xl max-sm:m-0 font-serif text-center'>ZenTasks - Manage your tasks at one place</h1>
 
-            <input value={todo} onChange={handleChange} className='w-[62%] max-md:w-[80%]' type="text" />
+            <div className="flex justify-center m-4 max-sm:mx-0 max-sm:my-2">
+              <h2 className='text-xl font-bold mx-2 max-sm:text-base max-sm:mx-1 max-sm:hidden'>Add a Todo</h2>
 
-            <button onClick={handleAdd} disabled={todo.length < 1} className='bg-slate-400 text-xl font-bold rounded-md mx-2 px-1 py-1 transition-all duration-400 disabled:opacity-30 hover:text-slate-300 hover:bg-slate-600 '><FaPlus /></button>
-          </div>
+              <input value={todo} onChange={handleChange} className='w-[62%] max-md:w-[80%] rounded-md px-2 text-cyan-950 font-bold bg-gray-400 opacity-70' type="text" />
 
-        </div>
-
-        <div className='bg-slate-800 opacity-20 mx-auto my-3 w-[90%] h-[1px]'></div>
-
-        <input onClick={toggleFinished} type="checkbox" value={showFinished} /> Show Finished
-
-        <h2 className="text-xl font-bold">Your Tasks:</h2>
-
-        <div className="todos bg-blackk max-h-[65vh] overflow-auto">
-
-          {todos.length === 0 && <div className='m-4 y text-2xl flex justify-center items-center'>No Todos to display</div>}
-
-          {todos.map(item => {
-
-            return (showFinished || !item.isCompleted) && < div key={item.id} className="todo my-2 px-2 flex justify-between items-center rounded-md bg-slate-300" >
-
-              <div className='flex max-w-[55vw] overflow-hidden'>
-                <input name={item.id} onChange={handleCheckbox} className='mx-2' type="checkbox" checked={item.isCompleted} />
-                <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
-              </div>
-
-              <div className="button">
-                <button onClick={(e) => handleEdit(e, item.id)} className='bg-slate-400 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-slate-300 hover:bg-slate-600 '><FaEdit /></button>
-                <button onClick={(e) => handleDelete(e, item.id)} className='bg-red-400 text-red-700 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-red-300 hover:bg-red-600 '><MdDeleteForever /></button>
-              </div>
-
+              <button onClick={handleAdd} disabled={todo.length < 1} className='bg-cyan-600 text-cyan-200 text-xl font-bold rounded-md mx-2 px-1 py-1 transition-all duration-400 disabled:opacity-30 hover:bg-cyan-300 hover:text-cyan-800  '><FaPlus /></button>
             </div>
 
-          })}
-        </div>
+          </div>
 
-      </div >
+          <div className='bg-cyan-400 opacity-60 mx-auto my-3 rounded-full w-[90%] h-[1px]'></div>
+          <div className="text-cyan-100 text-lg">
+            <input onClick={toggleFinished} type="checkbox" value={showFinished} /> Show Finished
+          </div>
+          <div className='flex justify-center'>
+            <h2 className="text-3xl font-bold text-cyan-100">Your Tasks</h2>
+          </div>
+          <div className="todos bg-blackk max-h-[65vh] overflow-auto">
+
+            {todos.length === 0 && <div className='text-cyan-200 opacity-50 m-4 y text-2xl flex justify-center items-center'>No Todos to display</div>}
+
+            {todos.map(item => {
+
+              return (showFinished || !item.isCompleted) && < div key={item.id} className="todo my-2 px-2 flex justify-between items-center rounded-md bg-cyan-600 hover:bg-cyan-700 opacity-80 text-lg transition-all duration-300 drop-shadow-2xl hover:drop-shadow-4xl" >
+
+                <div className='flex max-w-[55vw] overflow-hidden'>
+                  <input name={item.id} onChange={handleCheckbox} className='mx-2' type="checkbox" checked={item.isCompleted} />
+                  <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+                </div>
+
+                <div className="button">
+                  <button onClick={(e) => handleEdit(e, item.id)} className='bg-slate-400 text-slate-700 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-slate-300 hover:bg-slate-600 '><FaEdit /></button>
+                  <button onClick={(e) => handleDelete(e, item.id)} className='bg-red-400 text-red-700 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-red-300 hover:bg-red-600 '><MdDeleteForever /></button>
+                </div>
+
+              </div>
+
+            })}
+          </div>
+
+        </div >
+      </div>
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='flex bg-slate-600 justify-evenly items-center text-slate-200 h-16 '>
+    <nav className='flex bg-cyan-800 justify-evenly items-center text-cyan-200 h-16 '>
         <div className="logo">
-            <span className='font-bold text-2xl font-serif'>Z-Task</span>
+            <span className='font-bold text-2xl font-serif'>ZenTasks</span>
         </div>
         <ul className='flex gap-16 max-sm:gap-4'>
-            <li className='cursor-pointer text-xl max-sm:text-base transition-all duration-400 hover:text-slate-400  hover:border-b-8 hover:border-b-slate-400'>Home</li>
-            <li className='cursor-pointer text-xl max-sm:text-base transition-all duration-400 hover:text-slate-400  hover:border-b-8 hover:border-b-slate-400'>Tasks</li>
+            <li className='cursor-pointer text-xl max-sm:text-base font-semibold transition-all duration-500 hover:text-cyan-300  hover:tracking-wide'>Home</li>
+            <li className='cursor-pointer text-xl max-sm:text-base font-semibold transition-all duration-500 hover:text-cyan-300  hover:tracking-wide'>Tasks</li>
         </ul>
     </nav>
 
