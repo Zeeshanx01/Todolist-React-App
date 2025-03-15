6a24e57ee18f8c1b32a00e7c69b887fb0c4220b8
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2025-03-15 13:58:04 +0500

    25-03-15_Minor-design-changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [todo, setTodo] = useState("")
   //? Holds our all the todos:
   const [todos, setTodos] = useState([])
-  const [showFinished, setShowFinished] = useState(true)
+  const [showFinished, setShowFinished] = useState(false)
 
   //? saving your data to local storage, (keeps your todos even after refresh)
   useEffect(() => {
@@ -75,48 +75,48 @@ function App() {
 
   return (
     <>
-      <div className='min-h-screen bg-cyan-950'>
+      <div className='duration-300 min-h-screen bg-cyan-950'>
         <NavBar />
 
-        <div className="container mx-auto my-5 p-5 rounded-xl bg-cyan-900 opacity-85 min-h-[80vh] drop-shadow-2xl">
+        <div className="container mx-auto my-5 p-5  rounded-xl bg-cyan-900 opacity-85 h--[80vh] drop-shadow-2xl">
 
-          <div className="addTodo  text-cyan-100">
+          <div className="addTodo  text-cyan-100  sticky top-8 py-8 w-full bg-cyan-800 rounded-lg drop-shadow-2xl z-30">
 
-            <h1 className=' m-4 font-bold text-2xl max-sm:m-0 font-serif text-center'>ZenTasks - Manage your tasks at one place</h1>
+            <h1 className='duration-300  m-4 font-bold text-2xl max-sm:m-0 font-serif text-center'>ZenTasks - Manage your tasks at one place</h1>
 
             <div className="flex justify-center m-4 max-sm:mx-0 max-sm:my-2">
-              <h2 className='text-xl font-bold mx-2 max-sm:text-base max-sm:mx-1 max-sm:hidden'>Add a Todo</h2>
+              <h2 className='duration-300 text-xl font-bold mx-2 max-sm:text-base max-sm:mx-1 max-sm:hidden'>Add a Todo</h2>
 
-              <input value={todo} onChange={handleChange} className='w-[62%] max-md:w-[80%] rounded-md px-2 text-cyan-950 font-bold bg-gray-400 opacity-70 text-lg ' type="text" />
+              <input value={todo} onChange={handleChange} className='duration-300 w-[62%] max-md:w-[80%] rounded-md px-2 text-cyan-950 font-bold bg-gray-400 opacity-70 text-lg ' type="text" />
 
-              <button onClick={handleAdd} disabled={todo.length < 1} className='bg-cyan-600 text-cyan-200 text-xl drop-shadow-4xl font-bold rounded-md mx-2 px-1 py-1 transition-all duration-400 disabled:opacity-30 hover:bg-cyan-300 hover:text-cyan-800  '><FaPlus /></button>
+              <button onClick={handleAdd} disabled={todo.length < 1} className='duration-300 bg-cyan-600 text-cyan-200 text-xl drop-shadow-4xl font-bold rounded-md mx-2 px-1 py-1 transition-all duration-400 disabled:opacity-30 hover:bg-cyan-300 hover:text-cyan-800  '><FaPlus /></button>
             </div>
 
           </div>
 
-          <div className='bg-cyan-400 opacity-60 mx-auto my-3 rounded-full w-[90%] h-[1px]'></div>
+          <div className='duration-300 bg-cyan-400 opacity-60 mx-auto my-3 rounded-full w-[90%] h-[1px]'></div>
           <div className="text-cyan-100 text-lg">
-            <input onClick={toggleFinished} type="checkbox" value={showFinished} /> Show Finished
+            <input onClick={toggleFinished} type="checkbox" value={showFinished} /> Show Finished 
           </div>
-          <div className='flex justify-center'>
+          <div className='duration-300 flex justify-center'>
             <h2 className="text-3xl font-bold text-cyan-100">Your Tasks</h2>
           </div>
-          <div className="todos bg-blackk max-h-[65vh] overflow-auto">
+          <div className="todos bg-blackk max-h--[55vh] overflow-auto">
 
-            {todos.length === 0 && <div className='text-cyan-200 opacity-50 m-4 y text-2xl flex justify-center items-center'>No Todos to display</div>}
+            {todos.length === 0 && <div className='duration-300 text-cyan-200 opacity-50 m-4 y text-2xl flex justify-center items-center'>No Todos to display</div>}
 
             {todos.map(item => {
 
               return (showFinished || !item.isCompleted) && < div key={item.id} className="todo my-2 px-2 flex justify-between text-cyan-50 font-thin items-center rounded-md bg-cyan-600 hover:bg-cyan-700 opacity-80 text-lg transition-all duration-300 drop-shadow-2xl hover:drop-shadow-4xl" >
 
-                <div className='flex max-w-[55vw] overflow-hidden'>
-                  <input name={item.id} onChange={handleCheckbox} className='mx-2' type="checkbox" checked={item.isCompleted} />
+                <div className='duration-300 flex max-w-[55vw] overflow-hidden'>
+                  <input name={item.id} onChange={handleCheckbox} className='duration-300 mx-2' type="checkbox" checked={item.isCompleted} />
                   <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
                 </div>
 
                 <div className="button  drop-shadow-4xl">
-                  <button onClick={(e) => handleEdit(e, item.id)} className='bg-slate-400 text-slate-700 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-slate-300 hover:bg-slate-600 '><FaEdit /></button>
-                  <button onClick={(e) => handleDelete(e, item.id)} className='bg-red-400 text-red-700 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-red-300 hover:bg-red-600 '><MdDeleteForever /></button>
+                  <button onClick={(e) => handleEdit(e, item.id)} className='duration-300 bg-slate-400 text-slate-700 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-slate-300 hover:bg-slate-600 '><FaEdit /></button>
+                  <button onClick={(e) => handleDelete(e, item.id)} className='duration-300 bg-red-400 text-red-700 text-xl font-bold rounded-md mx-1 my-2 px-1 py-1 transition-all duration-400 hover:text-red-300 hover:bg-red-600 '><MdDeleteForever /></button>
                 </div>
 
               </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='flex bg-cyan-800 justify-evenly items-center  h-16 drop-shadow-xl '>
+    <nav className='flex bg-cyan-800 justify-evenly items-center  h-16 drop-shadow-2xl bg-opacity-40 stickytop-0 z-20 '>
         <div className="logo text-cyan-200">
             <span className='font-bold text-2xl font-serif'>ZenTasks</span>
         </div>
         <ul className='flex gap-16 max-sm:gap-4 opacity-70'>
             <li className='cursor-pointer text-xl max-sm:text-base font-semibold transition-all duration-500 text-cyan-50 hover:text-cyan-300  hover:tracking-wide'>Home</li>
-            <li className='cursor-pointer text-xl max-sm:text-base font-semibold transition-all duration-500 text-cyan-50 hover:text-cyan-300  hover:tracking-wide'>Tasks</li>
+            {/* <li className='cursor-pointer text-xl max-sm:text-base font-semibold transition-all duration-500 text-cyan-50 hover:text-cyan-300  hover:tracking-wide'>Tasks</li> */}
         </ul>
     </nav>
 
